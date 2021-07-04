ee9018ff19e005a149583b262e1be05ec41bd3ff
snehomoy100 <snehomoy100@gmail.com>
2021-07-04 20:40:04 +0530

    TalkNow parent component added

---
+++ talk-now/src/TalkNow.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./TalkNow.css"
+
+
+const TalkNow = () => {
+    return (
+        <div className="talknow">
+            
+        </div>
+    );
+}
+
+export default TalkNow;
