860ff3a67e20061fb2eecbecafed779f1649c6ff
snehomoy100 <snehomoy100@gmail.com>
2021-07-06 00:57:45 +0530

    css for Chat component added

--- talk-now/src/Chat.js
+++ talk-now/src/Chat.js
@@ -7,10 +7,23 @@ const Chat = () => {
     return(
         <div className="chat">
             {/* chat header  */}
+            <div className="chat__header">
+                <h4>To: <span className="chat__name">Channel Name</span></h4>
+                <strong>Details</strong>
+            </div>
 
             {/* chat body or messages  */}
+            <div className="chat__body">
+
+            </div>
+
+
 
             {/* chat input  */}
+            <div className="chat__input">
+
+            </div>
+
         </div>
     );
 };
